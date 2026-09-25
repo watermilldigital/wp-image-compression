c84b155eb0d1cf57458c2997993003280a6ac853
Ben <ben@watermilldigital.com>
2026-09-25 13:11:34 +0200

    Add Author header (WaterMill Digital); v2.1.1

--- wp-image-compression.php
+++ wp-image-compression.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Plugin Name: WP Image Compression
+ * Author: WaterMill Digital
+ * Author URI: https://watermilldigital.com
  * Description: Image compression: JPEG to WebP at a set quality, lossy PNG, and per-file savings tracking with a dashboard widget and Media Library column.
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
